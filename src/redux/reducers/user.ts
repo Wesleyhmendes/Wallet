09376b76b6e0e389eb56910ef81a8caea1d6c3cc
Wesley Mendes <wesleymendes123321@gmail.com>
2023-08-30 17:24:20 -0300

    requisito 1 refatorado

--- src/redux/reducers/user.ts
+++ src/redux/reducers/user.ts
@@ -3,16 +3,18 @@ import { AnyAction } from 'redux';
 import { USER_LOGIN } from '../actions';
 
 const INITIAL_STATE = {
-  user: {
-    email: '',
-    password: '',
-  },
+  email: '',
+  password: '',
 };
 
 const personalData = (state = INITIAL_STATE, action: AnyAction) => {
   switch (action.type) {
     case USER_LOGIN:
-      return action.payload;
+      return {
+        ...state,
+        email: action.payload.email,
+        password: action.payload.password,
+      };
     default:
       return state;
   }
